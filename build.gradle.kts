ff25fb8d40b7bd09ea7863f75aa7e04cfe9042e3
Chimbori <hello@chimbori.com>
2019-12-09 01:09:42 -0800

    Enable Kotlin for :animated-progress-bar module.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
+val kotlin_version: String by rootProject.extra
 plugins {
   id("com.android.library")
+  id("kotlin-android")
+  id("kotlin-android-extensions")
 }
 
 android {
@@ -13,4 +16,9 @@ android {
 dependencies {
   implementation("androidx.annotation:annotation:1.1.0")
   implementation("androidx.appcompat:appcompat:1.0.2")
+  implementation("androidx.core:core-ktx:+")
+  implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:$kotlin_version")
+}
+repositories {
+  mavenCentral()
 }
