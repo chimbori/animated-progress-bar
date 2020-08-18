7fc735100cf9061d584e307388657db8366f58f8
Chimbori <hello@chimbori.com>
2020-08-17 23:18:53 -0700

    Remove unnecessary direct dependencies from various modules.

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,12 +16,10 @@ android {
 
 dependencies {
   implementation("androidx.annotation:annotation:1.1.0")
-  implementation("androidx.appcompat:appcompat:1.0.2") {
-    because("DO NOT UPGRADE TO 1.1.0. It causes WebView crashes on Lollipop and Marshmallow.")
-  }
   implementation("androidx.core:core-ktx:1.3.0")
   implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:$kotlin_version")
 }
+
 repositories {
   mavenCentral()
 }
